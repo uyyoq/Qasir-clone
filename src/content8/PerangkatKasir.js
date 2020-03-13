3611f3ba058f229585ebd7119c83decd14864d67
tysni yuni lestari <tysniyuni@gmail.com>
2020-03-13 11:00:48 +0700

    merapihkan navbar

--- src/content8/PerangkatKasir.js
+++ src/content8/PerangkatKasir.js
@@ -6,18 +6,25 @@ export class PerangkatKasir extends Component {
     return (
       <div className="cont-pk">
         <div className="mesin1">
-          Si Kecil Praktis untuk Pedagang Dinamis Jika kamu mencari mesin kasir
-          portabel yang bisa dibawa ke mana-mana, ini dia jawabannya. Meski
-          kecil, perangkat ini sudah didukung dengan aplikasi printer thermal
-          dan plus scanner, Cocok buat para pengusaha yang punya mobilitas
-          tinggi, seperti kamu! SUNMI V1S jawabannya. Spesifikasi detail
+          <h2 className="1-bold">Si Kecil Praktis untuk Pedagang Dinamis</h2>
+          <p className="1-text">
+            Jika kamu mencari mesin kasir portabel yang bisa dibawa ke
+            mana-mana, ini dia jawabannya. Meski kecil, perangkat ini sudah
+            didukung dengan aplikasi printer thermal dan plus scanner, Cocok
+            buat para pengusaha yang punya mobilitas tinggi, seperti kamu! SUNMI
+            V1S jawabannya.
+          </p>
+          <p className="bttn-1">Spesifikasi detail</p>
         </div>
         <div className="mesin2">
-          Layar Besar untuk Kemudahan Transaksi Ini dia adalah solusi terbaik
-          untuk kamu yang membutuhkan mesin kasir berlayar lega. Layar 11
-          inch-nya akan memudahkanmu melakukan berbagai proses penjualan, mulai
-          dari kelola barang dagangan, sampai melakukan transaksi pembayaran.
-          Spesifikasi detail
+          <h2 className="2-bold">Layar Besar untuk Kemudahan Transaksi</h2>
+          <p className="2-text">
+            Ini dia adalah solusi terbaik untuk kamu yang membutuhkan mesin
+            kasir berlayar lega. Layar 11 inch-nya akan memudahkanmu melakukan
+            berbagai proses penjualan, mulai dari kelola barang dagangan, sampai
+            melakukan transaksi pembayaran.
+          </p>
+          <p className="bttn-2">Spesifikasi detail</p>
         </div>
       </div>
     );
