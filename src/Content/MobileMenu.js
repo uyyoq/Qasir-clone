bae10d26334309d75fbbbb270b49899300979fe8
tysni yuni lestari <tysniyuni@gmail.com>
2020-03-16 16:05:00 +0700

    merubah img di mobile menu

--- src/Content/MobileMenu.js
+++ src/Content/MobileMenu.js
@@ -19,14 +19,11 @@ export class MobileMenu extends Component {
           </p>
           <button className="Download">Download Sekarang</button>
         </div>
-        <div>
-          <img className="background" src={background} alt="img-mobile-menu" />
-          <div>
-            <img className="img-1" src={image1} alt="img-1" />
-            <img className="img-2" src={image2} alt="img-2" />
-            <img className="img-3" src={image3} alt="img-3" />
-            <img className="img-4" src={image4} alt="img-4" />
-          </div>
+        <div className="background">
+          <img className="img-1" src={image1} alt="img-1" />
+          <img className="img-2" src={image2} alt="img-2" />
+          <img className="img-3" src={image3} alt="img-3" />
+          <img className="img-4" src={image4} alt="img-4" />
         </div>
       </div>
     );
